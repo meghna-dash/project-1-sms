402ace1fe241cb17f73520e7d2cac09d99c448b0
meghna-dash <meghna@auburn.edu>
2019-10-17 02:34:19 -0500

    removed node modules

--- src/App.js
+++ src/App.js
@@ -5,20 +5,30 @@ import { Col, Button } from 'reactstrap';
 import AddProductPage from './pages/AddProductPage';
 import AddCustomerPage from './pages/AddCustomerPage';
 import AddPurchasePage from './pages/AddPurchasePage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Col>
-        <Link to="/add-product" /> 
-      </Col>
-      <Col>
-        <Link to="/add-customer" /> 
-      </Col>
-      <Col>
-        <Link to="/add-purchase" /> 
-      </Col>
-    </div>
+    <Router>
+      <div className="App">
+        <Col>
+          <Link to="/add-product" /> 
+        </Col>
+        <Col>
+          <Link to="/add-customer" /> 
+        </Col>
+        <Col>
+          <Link to="/add-purchase" /> 
+        </Col>
+      </div>
+    </Router>
   );
 }
 
